ea23c1e1227e01a8e0a149af526e3b446a70f974
ericfish12 <ericfish1221@gmail.com>
2021-02-20 01:13:12 -0500

    rg

--- server/db.js
+++ server/db.js
@@ -1,5 +1,37 @@
 const Sequelize = require('sequelize')
 
+
+var pg = require('pg');
+const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
+var client = new pg.Client({
+  user: "ericfish",
+  password: "guest",
+  database: "antra_todoList",
+  port: 5432,
+  host: "localhost",
+  ssl: false
+}); 
+client.connect();
+
+
 const db = new Sequelize (
     process.env.DATABASE_URL||`postgres://localhost:5432/antra_todoList`,  {
         logging: false
