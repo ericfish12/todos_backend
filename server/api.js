7385145390df96b136df40e4ae4d6544b70d38fd
ericfish12 <ericfish1221@gmail.com>
2021-02-19 20:52:44 -0500

    try

--- server/api.js
+++ server/api.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 // const { db,TodoLists } = require("./db.js");
 
 // router.get("/todos", async (req, res, next) => {
@@ -84,5 +84,47 @@ const router = require("express").Router();
 //   })
 
 
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+const router = require("express").Router();
+const { db,Messages } = require("./db.js");
+
+router.get("/messages", async (req, res, next) => {
+  try {
+    const messages = await Messages.findAll();
+    res.json(messages);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.post('/messages',async(req,res,next)=>{
+    try {
+        const newMessage = await Messages.create(req.body);
+        res.json(newMessage)
+
+  
+
+
+
+    } catch (error) {
+        // res.send(error)
+        // return res.sendStatus(400)
+        res.send('')
+        next(error)
+    }
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,104 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-app.use(morgan('dev'));
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const morgan = require('morgan');
+// app.use(morgan('dev'));
 
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', require('./api.js'))
-// you'll of course want static middleware so your browser can request things like your 'bundle.js'
-app.use(express.static(path.join(__dirname, '../public')))
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/api', require('./api.js'))
+// // you'll of course want static middleware so your browser can request things like your 'bundle.js'
+// app.use(express.static(path.join(__dirname, '../public')))
 
-// Any routes or other various middlewares should go here!
-app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
-// Make sure this is right at the end of your server logic!
-// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
-// (However, if you have middleware to serve up 404s, that go would before this as well)
-app.get('*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public'));
-});
-
-
+// // Any routes or other various middlewares should go here!
+// app.use((req, res, next) => {
+//     if (path.extname(req.path).length) {
+//       const err = new Error('Not found')
+//       err.status = 404
+//       next(err)
+//     } else {
+//       next()
+//     }
+//   })
+// // Make sure this is right at the end of your server logic!
+// // The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// // (However, if you have middleware to serve up 404s, that go would before this as well)
+// app.get('*', function (req, res, next) {
+//   res.sendFile(path.join(__dirname, '../public'));
+// });
 
 
 
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
+
+
+//   app.use((err, req, res, next) => {
+//     console.error(err)
+//     console.error(err.stack)
+//     res.status(err.status || 500).send(err.message || 'Internal server error.')
+//   })
   
 
 
 
-  app.listen(process.env.PORT || 3333, () =>
+//   app.listen(process.env.PORT || 3333, () =>
+//     console.log(`Mixing it up on port 3333`)
+//   )
+
+
+
+
+
+
+
+
+
+
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+// you'll of course want static middleware so your browser can request things like your 'bundle.js'
+app.use(morgan('dev'));
+
+
+app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  app.use('/api', require('./api.js'))
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Any routes or other various middlewares should go here!
+
+// Make sure this is right at the end of your server logic!
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+  // start listening (and create a 'server' object representing our server)
+app.listen(process.env.PORT || 3333, () =>
     console.log(`Mixing it up on port 3333`)
   )
